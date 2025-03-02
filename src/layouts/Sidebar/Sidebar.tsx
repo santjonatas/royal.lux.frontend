2837d7a9652f54ba259ffbdc6243bfd83c7ab491
santjonatas <jonatascaina100@gmail.com>
2025-03-02 08:52:36 -0300

    iniciando sidebar

--- src/layouts/Sidebar/Sidebar.tsx
+++ src/layouts/Sidebar/Sidebar.tsx
@@ -3,7 +3,7 @@ import './Sidebar.css'
 export default function Sidebar(){
     return(
         <aside id="sidebar-component">
-
+            <article id="img-royal-lux"></article>
         </aside>
     );
 }
